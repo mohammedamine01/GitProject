d83e326ec36f4de6ff6babe0e70a1188da77ee09
mohammed_amine <maennami@gmail.com>
2018-06-24 01:00:38 +0200

    initial cheking of the GitProject.

---
+++ GitProject/src/com/example/git/eclipse/Example.java
@@ -0,0 +1,13 @@
+package com.example.git.eclipse;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Example.printMessage();
+	}
+	
+	
+	public static void printMessage(){
+		System.out.println("Hello World!");
+	}
+}
